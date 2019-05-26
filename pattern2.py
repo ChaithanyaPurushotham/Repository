7bba278abe4b57e295d6245b03ff57012033d1af
ch <chaithanya319@outlook.com>
2019-05-26 15:24:16 +0530

    first commit

---
+++ pattern2.py
@@ -0,0 +1,21 @@
+num = int(input("enter the number"))
+space = num -1
+star = num - space
+for i in range(num):
+    for j in range(space):
+        print(" ",end="")
+    for k in range(star):
+        print("* ",end="")
+    star=star+1
+    space=space -1
+    print("")
+star = num-1
+space = num - star
+for i in range(num-1):
+    for j in range(space):
+        print(" ",end="")
+    for k in range(star):
+        print("* ",end="")
+    space = space + 1
+    star = star -1
+    print("")
